bd3f9699bc64ac85eef548845900fa89d2531bf4
Jimmy Kuruvilla <jimmyjk@gmail.com>
2014-12-16 15:44:03 -0500

    changed visual deletes

--- app/models/visual.rb
+++ app/models/visual.rb
@@ -7,8 +7,8 @@ class Visual < ActiveRecord::Base
 private
 
 def destroy_extra_visuals
-  if Visual.count >3000
-    Visual.select(:id).limit(1000).destroy_all
+  if Visual.count >500
+    Visual.select(:id).limit(100).destroy_all
   end
 
   response=`curl -X GET 'https://api.newrelic.com/v2/servers/6045686/metrics/data.json' \
@@ -18,7 +18,7 @@ def destroy_extra_visuals
   after_headers=response[response.index("{")..-1]
   max_time=JSON.parse(after_headers)["metric_data"]["metrics"][0]["timeslices"][-1]["values"]["max_response_time"]
   puts " @@@@@@@@@@@@@ MAX TIME: #{max_time} @@@@@@@@@@@@@@@"
-  
+
   if max_time >1000
     Instagram.delete_subscription(object: "all")
   end
